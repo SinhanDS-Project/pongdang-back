7885d2218164439177ec64f82be504d9b3668164
yoonminhyeok <nnnymh0729@gmail.com>
2025-09-02 12:27:34 +0900

    refactor: 게인게임 수정

--- src/main/java/com/wepong/pongdang/dto/request/PhoneVerificationSendRequestDTO.java
+++ src/main/java/com/wepong/pongdang/dto/request/PhoneVerificationSendRequestDTO.java
@@ -2,8 +2,13 @@ package com.wepong.pongdang.dto.request;
 
 import lombok.Getter;
 import jakarta.validation.constraints.NotBlank;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class PhoneVerificationSendRequestDTO {
-    @NotBlank private String phone;
+    @NotBlank
+    private String phone;
 }
