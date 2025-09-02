5dcb56763d82a09124692170333134d533f241cf
chaewon <codnjs257@naver.com>
2025-09-02 17:04:57 +0900

    feat: db config 파일 각각 추가(MariaDB, MySQL)

---
+++ src/main/java/com/wepong/pongdang/config/MySQLConfig.java
@@ -0,0 +1,69 @@
+package com.wepong.pongdang.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Profile("!test")
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        basePackages = "net.wepong.mysql.repository",
+        entityManagerFactoryRef = "secondEntityManagerFactory",
+        transactionManagerRef = "secondTransactionManager"
+)
+public class MySQLConfig {
+
+    @Bean
+    @ConfigurationProperties("mysql.datasource")
+    public DataSourceProperties mysqlDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean(name = "mysqlDataSource")
+    public DataSource mysqlDataSource(@Qualifier("mysqlDataSourceProperties") DataSourceProperties props) {
+        return props.initializeDataSourceBuilder().build();
+    }
+
+    @Bean(name = "secondJpaPropertyMap")
+    @ConfigurationProperties(prefix = "mysql.jpa.properties") // 표준 키들(hbm2ddl.auto 등)
+    public Map<String, Object> secondJpaPropertyMap() {
+        return new HashMap<>();
+    }
+
+    @Bean(name = "secondEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean secondEntityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("mysqlDataSource") DataSource dataSource,
+            @Qualifier("secondJpaPropertyMap") Map<String, Object> props
+    ) {
+        props.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        return builder
+                .dataSource(dataSource)
+                .packages("net.wepong.mysql.entity")
+                .persistenceUnit("second")
+                .properties(props) // Map 한 방으로 끝
+                .build();
+    }
+
+    @Bean(name = "secondTransactionManager")
+    public PlatformTransactionManager secondTransactionManager(
+            @Qualifier("secondEntityManagerFactory") EntityManagerFactory emf
+    ) {
+        return new JpaTransactionManager(emf);
+    }
+}
