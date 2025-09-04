4e48958c0651bde513a8799481ac4035847841fb
chaewon <codnjs257@naver.com>
2025-09-04 10:28:45 +0900

    edit: 금액 관련 자료형 변경

--- src/main/java/com/wepong/pongdang/dto/request/FinanceRequestDTO.java
+++ src/main/java/com/wepong/pongdang/dto/request/FinanceRequestDTO.java
@@ -10,12 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class FinanceRequestDTO {
+    private String name; // 이름
     private int age; // 나이
-    private int income; // 월 소득
-    private int spend; // 월 소비
+    private long income; // 월 소득
+    private long spend; // 월 소비
     private String main_category; // 주요 소비 항목
-    private int saving_goal; // 목표 저축액
-    private int current_saving; // 현재 저축액
+    private long saving_goal; // 목표 저축액
+    private long current_saving; // 현재 저축액
     private String loan; // 부채 여부
     private String invest_type; // 안정형, 중립형, 공격형
     private String goal_term; // 목표 기간
