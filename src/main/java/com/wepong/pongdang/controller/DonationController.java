1abe0db56d51ec3b4e81126ca90967d1684136eb
ryuchaerin <dmsquf1009@dgu.ac.kr>
2025-09-05 10:09:48 +0900

    :zap: edit: 사용자 로그인 예외 처리

--- src/main/java/com/wepong/pongdang/controller/DonationController.java
+++ src/main/java/com/wepong/pongdang/controller/DonationController.java
@@ -3,6 +3,7 @@ package com.wepong.pongdang.controller;
 import com.wepong.pongdang.dto.request.DonationRequestDTO;
 import com.wepong.pongdang.dto.response.DonationInfoResponseDTO;
 import com.wepong.pongdang.dto.response.DonationResponseDTO;
+import com.wepong.pongdang.exception.UnauthorizedAccessException;
 import com.wepong.pongdang.service.AuthService;
 import com.wepong.pongdang.service.DonationService;
 import lombok.RequiredArgsConstructor;
@@ -31,7 +32,10 @@ public class DonationController {
 
     @PostMapping("")
     public DonationResponseDTO donate(@RequestBody DonationRequestDTO donationRequestDTO,
-                                      @RequestHeader("Authorization") String authHeader) {
+                                      @RequestHeader(value = "Authorization", required = false) String authHeader) {
+        if (authHeader == null || authHeader.isBlank()) {
+            throw new UnauthorizedAccessException(); // "로그인 후 이용이 가능한 서비스입니다"
+        }
         Long userId = authService.validateAndGetUserId(authHeader);
         return donationService.pongDonate(donationRequestDTO, userId);
     }
