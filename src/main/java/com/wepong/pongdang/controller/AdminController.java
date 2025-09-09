e19fa77eeed771a5c3e5f6715bb1b2d5f2450c65
ryuchaerin <dmsquf1009@dgu.ac.kr>
2025-09-09 11:05:51 +0900

    :sparkles: feat: 관리자 기능 클래스 정의

---
+++ src/main/java/com/wepong/pongdang/controller/AdminController.java
@@ -0,0 +1,11 @@
+package com.wepong.pongdang.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin")
+public class AdminController {
+}
