956306064d1d11ae30c8326dfba50697dab8d5ca
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2025-09-09 22:50:27 +0900

    :fire: edit: 불필요 Enum 삭제

--- src/main/java/com/wepong/pongdang/entity/enums/PongHistoryType.java
+++ src/main/java/com/wepong/pongdang/entity/enums/PongHistoryType.java
@@ -1,5 +1,5 @@
 package com.wepong.pongdang.entity.enums;
 
 public enum PongHistoryType {
-    CHARGE, GAME, GAME_P, GAME_D, PURCHASE, DONATION_P, DONATION_D, ADD
+    CHARGE, GAME_P, GAME_D, PURCHASE, DONATION_P, DONATION_D, ADD
 }
