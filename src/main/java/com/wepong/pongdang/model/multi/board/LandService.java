0649556d597a4f0ba752191c7ada1a1cabba3a5d
ryuchaerin <dmsquf1009@dgu.ac.kr>
2025-09-24 09:53:40 +0900

    :memo: edit: 땅 정보 수정

--- src/main/java/com/wepong/pongdang/model/multi/board/LandService.java
+++ src/main/java/com/wepong/pongdang/model/multi/board/LandService.java
@@ -22,17 +22,17 @@ public class LandService {
 
     public void setLands(Long roomId) {
         List<LandDTO> lands = new ArrayList<>();
-        lands.add(new LandDTO(0, "출발", 0, 30, null, null));
+        lands.add(new LandDTO(0, "출발", 15, 0, null, null));
         lands.add(new LandDTO(1, "케이뱅크", 3, 6, null, null));
         lands.add(new LandDTO(2, "토스뱅크", 3, 6, null, null));
-        lands.add(new LandDTO(3, "퀴즈", 0, 0, null, null));
+        lands.add(new LandDTO(3, "퀴즈", 5, 5, null, null));
         lands.add(new LandDTO(4, "수협은행", 4, 8, null, null));
         lands.add(new LandDTO(5, "카카오뱅크", 4, 8, null, null));
-        lands.add(new LandDTO(6, "무인도", 0, 0, null, null));
+        lands.add(new LandDTO(6, "휴게소", 0, 0, null, null));
         lands.add(new LandDTO(7, "한국씨티은행", 5, 10, null, null));
         lands.add(new LandDTO(8, "HSBC", 5, 10, null, null));
         lands.add(new LandDTO(9, "웰스파고", 6, 12, null, null));
-        lands.add(new LandDTO(10, "퀴즈", 0, 0, null, null));
+        lands.add(new LandDTO(10, "퀴즈", 5, 5, null, null));
         lands.add(new LandDTO(11, "IM뱅크", 6, 12, null, null));
         lands.add(new LandDTO(12, "금고", 0, 0, null, null));
         lands.add(new LandDTO(13, "SC제일은행", 7, 14, null, null));
@@ -40,7 +40,7 @@ public class LandService {
         lands.add(new LandDTO(15, "저금", 0, 5, null, null));
         lands.add(new LandDTO(16, "NH농협은행", 8, 16, null, null));
         lands.add(new LandDTO(17,  "IBK기업은행", 8, 16, null, null));
-        lands.add(new LandDTO(18, "여행", 0, 0, null, null));
+        lands.add(new LandDTO(18, "보너스", 0, 0, null, null));
         lands.add(new LandDTO(19, "우리은행", 9, 18, null, null));
         lands.add(new LandDTO(20, "하나은행", 9, 18, null, null));
         lands.add(new LandDTO(21, "KB국민은행", 9, 18, null, null));
